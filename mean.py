9b0c30179b6780a507886a27438d1b81fa0a4cc0
Daniela Knoefler <knoefler@umich.edu>
2014-07-20 10:27:35 -0400

    Change variable n to x

--- mean.py
+++ mean.py
@@ -2,15 +2,20 @@
 import sys
 
 sum = 0
-n = 0
+x = 0
+
+# x instaed of n
 
 #Sum input values
 for num in open('data.txt'):
 	sum += float(num)
-	n += 1
+	x += 1
 
 
 #output:
-print sum / n
+print sum / x
 
-print n
+
+print x
+
+
